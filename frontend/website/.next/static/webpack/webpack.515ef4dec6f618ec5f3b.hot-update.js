a1f4fb6850879829d1aa68e5da00a7c6081e8709
cheboi <moscheb96@gmail.com>
2021-11-17 12:49:03 +0300

    updated commit

---
+++ frontend/website/.next/static/webpack/webpack.515ef4dec6f618ec5f3b.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "51bd20d5ca1fe5b305b5"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay41MTVlZjRkZWM2ZjYxOGVjNWYzYi5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjUxYmQyMGQ1Y2ExZmU1YjMwNWI1XCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
